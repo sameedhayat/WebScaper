6b09fe987180250d852dce2b45d77181fdb3831e
sameedhayat <sameedhayat@gmail.com>
2018-06-24 12:47:47 +0200

    current directory path added

--- scratch.py
+++ scratch.py
@@ -1,25 +1,7 @@
 from google.transit import gtfs_realtime_pb2 as gtfs_rt
-# import wget
-# from datetime import datetime, timedelta
+
 from time import gmtime, strftime
-# def daterange(start_date, end_date):
-#     start_date = datetime.strptime(start_date, '%Y-%m-%d')
-#     end_date = datetime.strptime(end_date, '%Y-%m-%d')
-#     for n in range(int ((end_date - start_date).days)+1):
-#         strg = '{:%Y-%m-%d}'.format(start_date + timedelta(n))
-#         yield strg
-#
-# start_date = '2018-06-16'
-# # finish_date = '2018-06-17'
-# # for r in daterange(start_date, finish_date):
-# #     url = "https://datamine-%s.s3.amazonaws.com/gtfs.tgz" % r
-# #     filename = wget.download(url, 'gtfs_%s.tgz' % r)
-# key = 'e52f0769ccdce73f961a6332beb2033e'
-# url = "https://datamine-%s.s3.amazonaws.com/gtfs.tgz" % start_date
-# print 'gtfs_%s.tgz' % strftime("%Y-%m-%d-%H-%M-%S", gmtime())
-# filename = wget.download(url, 'gtfs_%s.tgz' % strftime("%Y-%m-%d-%H-%M-%S", gmtime()))
 import os
-import errno
 import requests
 
 def create_directories(filenames):
@@ -48,6 +30,7 @@ filenames = ['busVehiclePosition','bustripUpdates',
 create_directories(filenames)
 
 timestamp = strftime("%Y-%m-%d-%H-%M-%S", gmtime())
+curr_dir = os.path.dirname(os.path.abspath(__file__))
 feed = gtfs_rt.FeedMessage()
 
 for name, url in zip(filenames, urls):
@@ -55,18 +38,10 @@ for name, url in zip(filenames, urls):
         response = requests.get(url)
         feed.ParseFromString(response.content)
         file_name = '%s_%s' % (name,timestamp)
-        file_path = os.path.join(name, file_name)
+        file_path = os.path.join(curr_dir, "data", name, file_name)
 
         with open(file_path, "w") as f:
             f.writelines(repr(feed))
     except:
         pass
-
-
-# with open(file_name, "w") as f:
-#     f.writelines(repr(feed))
-
-# for entity in feed.entity:
-#   if entity.HasField('trip_update'):
-#     print entity.trip_update
 
